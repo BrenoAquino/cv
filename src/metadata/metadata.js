c565edfb26dafbe1eb31a8d7f1611da5985af569
Breno Aquino <brenoaquino7phr@gmail.com>
2021-12-30 10:26:10 -0300

    inital commit

---
+++ src/metadata/metadata.js
@@ -0,0 +1,111 @@
+module.exports = {
+  name: 'Breno Aquino',
+  title: 'Software Engineer',
+  facts: {
+    'Residence': 'Fortaleza, CE',
+    'GitHub': '<a href="https://github.com/BrenoAquino"><i class="fab fa-github fact-icon"></i>brenoaquino</a>',
+    'Email': '<a href="mailto:brenoaquino7phr@gmail.com">brenoaquino7phr@gmail.com</a>'
+  },
+  overview: 'Sou uma pessoa que sempre busca inovação e automação de processos. Tive experiencias com projetos na área de educação e com Streaming. Trabalho com foco na melhoria de performance, na criação e evolução da arquitetura, melhoria de qualidade e a própria evolução de produto. Sempre que possível crio ferramentas para automatizar processos repetitivos.',
+  skills: [
+    ['Swift', '+++++'],
+    ['iOS', '+++++'],
+    ['Git', '+++++'],
+    ['Fastlane', '++++'],
+    ['GithubActions', '++++'],
+    ['Bitrise', '++++'],
+
+    ['Flutter', '++']
+
+    ['Python', '+++++'],
+    ['Crawling', '+++++'],
+    ['Flask', '+++'],
+    ['TensorFlow', '+++'],
+
+    ['Node.js', '+++'],
+
+    ['AWS', '++++'],
+    ['Scrum', '+++++'],
+  ],
+  positions: [
+    {
+      company: 'Meta',
+      location: 'Fortaleza - CE',
+      title: 'iOS Developer Senior',
+      period: 'Dezembro de 2020 - momento (meio período)',
+      skills: ['iOS', 'Swift', 'GithubActions', 'Scrum'],
+      contents: `
+Meta desde dezembro de 2020: Fui responsável pela estruturação do projeto de iOS com o cliente MLSE. Junto com um outro desenvolvedor Android nos estruturamos a parte técnica do projeto. Para iOS, foi construído seguindo Clean Architecture e utilizando o MVVM. Um dos desafios era fornecer uma interface de interação pro usuário alinhada com os acontecimentos do jogo. Diferente da maioria dos projetos, esse teve o objetivo de criar um SDK (distribuída com cocoapods para iOS) para ser integrada em outro apps. 
+
+Nós seguimos o Scrum e como se tratava de um cliente canadense, tínhamos algumas cerimônias/reuniões de alinhamentos com eles realizadas em inglês.`
+    },
+    {
+      company: 'Claro Brasil',
+      location: 'São Paulo - SP',
+      title: 'iOS Developer Senior',
+      period: 'Março de 2019 - momento',
+      skills: ['iOS', 'Swift', 'Fastlane', 'Bitrise', 'Scrum'],
+      contents: `
+Trabalho com um projeto de Streaming para fornecer uma versão mobile dos conteúdos VOD e Live. Temos foco em performance, modularização e qualidade de entregas. Criamos arquiteturas para melhorar os ganhos nessas áreas e facilitar a manutenção e evolução do produto.`
+    },
+    {
+      company: 'Universidade de Fortaleza',
+      location: 'Fortaleza - CE',
+      title: 'iOS Developer',
+      period: 'Março de 2018 - Março de 2019',
+      skills: ['iOS', 'Swift', 'Liderança Técnica', 'Ruby on Rails'],
+      contents: `
+Trabalhei como líder e como desenvolvedor iOS. Os principal aplicativo que trabalhei foi o Unifor Mobile, uma versão mobile (tanto Android quanto iOS) da plataforma da universidade para alunos, professores e funcionário. Auxiliei na criação de outros aplicativos, como RadioNIC (um podcast feito pelos alunos da própria universidade), como líder de uma equipe de estagiários, focando em ensiná-los a construir um aplicativo. Ministrei curso para estagiários alunos que eram contratados pela Universidade.`
+    },
+    {
+      company: 'Universidade de Fortaleza',
+      location: 'Fortaleza - CE',
+      title: 'Estagiário - iOS Developer',
+      period: 'Julho de 2017 - Março de 2018',
+      skills: ['iOS', 'Swift', 'UX'],
+      contents: `
+Entrei como estagiário de desenvolvimento iOS e trabalhei na reconstrução do aplicativo da Universidade. Tive experiencia com diversas áreas como desenvolvimento iOS, desenvolvimento Android, desenvolvimento com Ruby on Rails, líder técnico e UX.`
+    },
+  ],
+  education: [
+    {
+      company: 'Universidade de Fortaleza',
+      level: 'Graduação',
+      course: 'Engenharia de Computação',
+      period: '2015 - 2019',
+      contents: `
+* Trabalho de Conclusão de Curso: Pesquisa utilizando Redes Neurais Recorrentes para predição de tendencias em series de criptomoedas.
+* Atividades: Monitor da disciplina de Programação orientada a objetos em 2016; monitor da disciplina de microprocessadores em 2017; bolsista de iniciação científica na área se seguranças em redes de sensores.`
+    }
+  ],
+  experience: [
+    {
+      header: 'iOS Movies',
+      skills: ['iOS', 'Swift', 'Design System', 'MVVM', 'View Code'],
+      contents: `
+WIP
+`
+    },
+    {
+      header: 'Home Security System',
+      skills: ['Computer Vision', 'Python', 'Flask', 'WebSocket', 'MongoDB', 'Raspberry Pi'],
+      contents: `
+WIP
+`
+    },
+    {
+      header: 'Canal Eletronico do Investidor B3 - Crawler',
+      skills: ['NodeJS', 'Python', 'Serverless', 'AWS DynamoDB', 'AWS Lambda'],
+      contents: `
+WIP
+`
+    },
+    {
+      header: 'Mobile Stores (App Store e Play Store) - Crawler',
+      skills: ['Python', 'Serverless', 'AWS Step Functions', 'AWS Lambda', 'AWS DynamoDB'],
+      contents: `
+WIP
+`
+    }
+  ]
+};
