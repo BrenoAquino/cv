ce82e880ad32296bc53e8b46b2a5fc6b879f9768
Breno Aquino <brenoaquino7phr@gmail.com>
2022-05-16 10:45:57 -0300

    Update metadata-en.js

--- src/metadata/metadata-en.js
+++ src/metadata/metadata-en.js
@@ -26,19 +26,29 @@ module.exports = {
   ],
   positions: [
     {
-      company: 'Meta',
-      location: 'Remoto',
+      company: 'Itaú',
+      location: 'Remote',
       title: 'iOS Developer Senior',
-      period: 'Dezembro de 2020 - momento (meio período)',
+      period: 'March 2022 - current',
+      skills: ['iOS', 'Swift', 'Modules'],
+      contents: `
+`
+    },
+    {
+      company: 'Meta',
+      location: 'Remote',
+      title: 'iOS Developer Senior',
+      period: 'December de 2020 - Febuary 2022 (part time)',
       skills: ['iOS', 'Swift', 'Github Actions', 'Scrum'],
       contents: `
-Fui responsável pela estruturação do projeto junto com o cliente (Maple Leaf Sports & Entertainment). Junto com um outro desenvolvedor Android nós estruturamos a parte técnica do projeto. Para o projeto iOS utilizamos Clean Architecture e MVVM. O grande desafio desse projeto, diferente da maioria, foi criar não um aplicativo, mas sim uma SDK (distribuído com Cocoapods) para ser integrada em outros aplicativos. O projeto seguiu a metodologia agil utilizando Scrum. Como o cliente era no Canadá, as cerimônias eram realizadas em inglês.`
+Fui responsável pela estruturação do projeto junto com o cliente (Maple Leaf Sports & Entertainment). Junto com um outro desenvolvedor Android nós estruturamos a parte técnica do projeto. Para o projeto iOS utilizamos Clean Architecture e MVVM. O grande desafio desse projeto, diferente da maioria, foi criar não um aplicativo, mas sim uma SDK (distribuído com Cocoapods) para ser integrada em outros aplicativos. O projeto seguiu a metodologia agil utilizando Scrum. Como o cliente era no Canadá, as cerimônias eram realizadas em inglês.
+`
     },
     {
       company: 'Claro Brasil',
       location: 'São Paulo - SP',
       title: 'iOS Developer Senior',
-      period: 'Março de 2019 - momento',
+      period: 'March de 2019 - March 2022',
       skills: ['iOS', 'Swift', 'Fastlane', 'Bitrise', 'SAFe'],
       contents: `
 Trabalho em projetos de streaming fornecendo para o cliente conteúdos VOD e Live. Os projetos seguem a metodologia agil utilizando SAFe. O principal projeto é o Now Online, com o foco em melhorar a qualidade do produto e prover uma melhor experiência para o usuário, juntamente com a melhoria da arquitetura do projeto.
@@ -46,22 +56,24 @@ Trabalho em projetos de streaming fornecendo para o cliente conteúdos VOD e Liv
     },
     // TODO: Merge Universidade de Fortaleza
     {
-      company: 'Universidade de Fortaleza',
-      location: 'Fortaleza - CE',
+      company: 'University of Fortaleza - UNIFOR',
+      location: 'Fortaleza - CE, Brazil',
       title: 'iOS Developer',
-      period: 'Março de 2018 - Março de 2019',
+      period: 'March 2018 - March 2019',
       skills: ['iOS', 'Swift', 'Liderança Técnica', 'Ruby on Rails'],
       contents: `
-Trabalhei atuando como líder técnico e como desenvolvedor iOS. O principal aplicativo que trabalhei foi o Unifor Mobile, uma versão mobile da plataforma da universidade para alunos, professores e funcionários. Dentre as funcionalidades, se destacada a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade. Outra atividade de destaque era a formação de estagiários para o mercado de trabalho por meio de treinamentos e acompanhamentos diários. Também auxiliei na criação de outros aplicativos, como RadioNIC, um podcast feito pelos alunos da própria universidade.`
+Trabalhei atuando como líder técnico e como desenvolvedor iOS. O principal aplicativo que trabalhei foi o Unifor Mobile, uma versão mobile da plataforma da universidade para alunos, professores e funcionários. Dentre as funcionalidades, se destacada a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade. Outra atividade de destaque era a formação de estagiários para o mercado de trabalho por meio de treinamentos e acompanhamentos diários. Também auxiliei na criação de outros aplicativos, como RadioNIC, um podcast feito pelos alunos da própria universidade.
+`
     },
     {
-      company: 'Universidade de Fortaleza',
-      location: 'Fortaleza - CE',
-      title: 'Estagiário - iOS Developer',
-      period: 'Julho de 2017 - Março de 2018',
+      company: 'University of Fortaleza - UNIFOR',
+      location: 'Fortaleza - CE, Brazil',
+      title: 'Intern - iOS Developer',
+      period: 'July 2017 - March 2018',
       skills: ['iOS', 'Swift', 'UX'],
       contents: `
-Fui estagiário de desenvolvimento iOS e trabalhei na reconstrução do aplicativo da Universidade. Tive experiência com diversas áreas como desenvolvimento iOS, desenvolvimento Android, desenvolvimento com Ruby on Rails e UX.`
+Fui estagiário de desenvolvimento iOS e trabalhei na reconstrução do aplicativo da Universidade. Tive experiência com diversas áreas como desenvolvimento iOS, desenvolvimento Android, desenvolvimento com Ruby on Rails e UX.
+`
     },
   ],
   education: [
@@ -72,7 +84,8 @@ Fui estagiário de desenvolvimento iOS e trabalhei na reconstrução do aplicati
       period: '2015 - 2019',
       contents: `
 * Closure Project: Research using recurrent neural networks to predict criptocurrency trends.
-* Activities: Object Orientations Monitor in 2016; Microprocessors Monitor in 2017; Researcher in security area of network sensors area.`
+* Activities: Object Orientations Monitor in 2016; Microprocessors Monitor in 2017; Researcher in security area of network sensors area.
+`
     }
   ],
   personal_projects: [
