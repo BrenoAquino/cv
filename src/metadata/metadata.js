cdeabf8e24bd650f0640bd051e8b5d9ad8d2aca3
Breno Aquino <brenoaquino7phr@gmail.com>
2023-10-17 18:09:56 -0300

    Update metadata.js

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -34,9 +34,9 @@ module.exports = {
       period: 'October 2022 - current',
       skills: ['iOS', 'Swift', 'Objective C', 'UIKit', 'View Code', 'SwiftUI', 'Unit Testing', 'XCTest', 'Modules', 'Github Actions', 'Xcode Cloud', 'Fastlane', 'SDK'],
       contents: `
-      * Responsável por projetar e criar uma SDK personalizado para o analytics da empresa;
-      * Implementei um recurso de autenticação que é modificado dependendo do país do usuário;
-      * Implementei um módulo de privacidade para internacionalizar o aplicativo e seguir as leis de cada país/estado.
+* Responsável por projetar e criar uma SDK personalizado para o analytics da empresa;
+* Implementei um recurso de autenticação que é modificado dependendo do país do usuário;
+* Implementei um módulo de privacidade para internacionalizar o aplicativo e seguir as leis de cada país/estado.
       `
     },
     {
@@ -46,9 +46,9 @@ module.exports = {
       period: 'March 2022 - October 2022',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'XCTest', 'Modules', 'Swift Package Manager', 'Cocoapods', 'Jenkins', 'Fastlane'],
       contents: `
-      * Responsável por liderar e orientar um desenvolvedor Junior, ensinando e orientando no desenvolvimento iOS;
-      * Implementei vários módulos individuais e distribuídos usando Cocoapods;
-      * Responsável por manter todo o fluxo de notificações, composto por push notifications, deeplinks e uma funcionalidade que permite ao usuário acompanhar suas notificações.
+* Responsável por liderar e orientar um desenvolvedor Junior, ensinando e orientando no desenvolvimento iOS;
+* Implementei vários módulos individuais e distribuídos usando Cocoapods;
+* Responsável por manter todo o fluxo de notificações, composto por push notifications, deeplinks e uma funcionalidade que permite ao usuário acompanhar suas notificações.
       `
     },
     {
@@ -58,11 +58,11 @@ module.exports = {
       period: 'December 2020 - February 2022 (part time)',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'WebSocket', 'SDK', 'Cocoapods', 'CI/CD', 'Github Actions', 'Scrum'],
       contents: `
-      * Atuei como líder criando e definindo o projeto de estrutura base para o cliente Maple Leaf Sports & Entertainment (MLSE). Definindo a prioridade de funcionalidades, arquiteturas utilizadas, métricas, APIs e web socket para comportamento em tempo real;
-      * Implementei uma SDK seguindo Clean e MVVM para fornecer recursos como transmissões ao vivo e VOD, chats, estatísticas em tempo real, mini jogos e outros recursos e distribuído usando Cocoapods;
-      * Implementei um design system e um tema personalizado a ser definido pelo aplicativo host;
-      * Implementei scripts e configurei o CI (usando Github Action) para automatizar a construção e distribuição do SDK compilado e sua documentação;
-      * Utilizei metodologias Agile e Scrum, realizando também reuniões semanais com o cliente e a equipe de backend do cliente.
+* Atuei como líder criando e definindo o projeto de estrutura base para o cliente Maple Leaf Sports & Entertainment (MLSE). Definindo a prioridade de funcionalidades, arquiteturas utilizadas, métricas, APIs e web socket para comportamento em tempo real;
+* Implementei uma SDK seguindo Clean e MVVM para fornecer recursos como transmissões ao vivo e VOD, chats, estatísticas em tempo real, mini jogos e outros recursos e distribuído usando Cocoapods;
+* Implementei um design system e um tema personalizado a ser definido pelo aplicativo host;
+* Implementei scripts e configurei o CI (usando Github Action) para automatizar a construção e distribuição do SDK compilado e sua documentação;
+* Utilizei metodologias Agile e Scrum, realizando também reuniões semanais com o cliente e a equipe de backend do cliente.
       `
     },
     {
@@ -72,12 +72,12 @@ module.exports = {
       period: 'March 2019 - March 2022',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'Swift Package Manager', 'CI/CD', 'Fastlane', 'Bitrise', 'SAFe'],
       contents: `
-      * Desenvolvi um projeto de stream desde o início e aprimorei outro projeto de stream, chamado NOW Online. Ambos fornecem conteúdos ao vivo e VOD;
-      * Trabalhei como líder de outros 2 desenvolvedores iOS definindo as tecnologias, arquitetura e prioridades;
-      * Ajudei a definir a solução e a arquitetura do projeto, sempre buscando performance;
-      * Implementei um recurso para listar todos os canais e seus programas, para melhorar o desempenho e o tempo de renderização da tela para o usuário, implementei um algoritmo de cache customizado para evitar recálculo desnecessários e busca binária para melhorar o tempo para encontrar o programa atual baseado em na hora atual;
-      * Melhorou a arquitetura para usar MVVM e seguir o Clean Architecture no projeto NOW, além de ter começado a implementar testes unitários. Este aplicativo tinha cerca de 300.000+ usuários ativos mensais;
-      * Aumentei a cobertura de testes aplicando e apliquei o conceito de injeção de dependência.
+* Desenvolvi um projeto de stream desde o início e aprimorei outro projeto de stream, chamado NOW Online. Ambos fornecem conteúdos ao vivo e VOD;
+* Trabalhei como líder de outros 2 desenvolvedores iOS definindo as tecnologias, arquitetura e prioridades;
+* Ajudei a definir a solução e a arquitetura do projeto, sempre buscando performance;
+* Implementei um recurso para listar todos os canais e seus programas, para melhorar o desempenho e o tempo de renderização da tela para o usuário, implementei um algoritmo de cache customizado para evitar recálculo desnecessários e busca binária para melhorar o tempo para encontrar o programa atual baseado em na hora atual;
+* Melhorou a arquitetura para usar MVVM e seguir o Clean Architecture no projeto NOW, além de ter começado a implementar testes unitários. Este aplicativo tinha cerca de 300.000+ usuários ativos mensais;
+* Aumentei a cobertura de testes aplicando e apliquei o conceito de injeção de dependência.
       `
     },
     {
@@ -87,11 +87,11 @@ module.exports = {
       period: 'March 2018 - March 2019',
       skills: ['iOS', 'Swift', 'UIKit', 'Storyboard', 'View Code', 'UI Testing', 'MVC', 'MVVM', 'Tech Lead', 'Ruby on Rails', 'Fastlane'],
       contents: `
-      * Atuei como Tech Lead para uma equipe de 10 estagiários;
-      * Fui responsável por todos os aplicativos iOS da universidade. O principal aplicativo da universidade tinha mais de 24.000 usuários ativos mensais;
-      * Ministrei cursos de iOS para estagiários e estudantes;
-      * Responsável por ensinar sobre a metodologia iOS, git, scrum e também como desenhar toda a solução, desde UX, prioridades, APIs, front end até testes;
-      * Liderei a refatoração do principal aplicativo da universidade seguindo MVVM. Dentre as funcionalidades, se destaca a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade.
+* Atuei como Tech Lead para uma equipe de 10 estagiários;
+* Fui responsável por todos os aplicativos iOS da universidade. O principal aplicativo da universidade tinha mais de 24.000 usuários ativos mensais;
+* Ministrei cursos de iOS para estagiários e estudantes;
+* Responsável por ensinar sobre a metodologia iOS, git, scrum e também como desenhar toda a solução, desde UX, prioridades, APIs, front end até testes;
+* Liderei a refatoração do principal aplicativo da universidade seguindo MVVM. Dentre as funcionalidades, se destaca a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade.
       `
     },
   ],
@@ -102,10 +102,10 @@ module.exports = {
       course: 'Computing Engineering',
       period: '2015 - 2019',
       contents: `
-      * Trabalho de Conclusão de Curso: Pesquisa usando uma rede neural recorrente para tentar predizer a tendência de criptomoedas.
-      * Monitor da disciplina de Programação Orientada a Objetos em 2016.
-      * Monitor da disciplina de Microprocessor em 2017.
-      * Pesquisa na área de segurança em redes de sensores, tentando implementar um algoritmo para detecção de ataques.
+* Trabalho de Conclusão de Curso: Pesquisa usando uma rede neural recorrente para tentar predizer a tendência de criptomoedas.
+* Monitor da disciplina de Programação Orientada a Objetos em 2016.
+* Monitor da disciplina de Microprocessor em 2017.
+* Pesquisa na área de segurança em redes de sensores, tentando implementar um algoritmo para detecção de ataques.
       `
     }
   ],
