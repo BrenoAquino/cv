9abfe3a2cf6405fe81bc52c10aac1e909fca4982
rr-breno-aquino <os-breno.aquino@rakuten.com>
2024-02-06 01:11:41 -0300

    updating CV

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,10 +1,10 @@
 module.exports = {
   name: 'Breno Aquino',
-  title: 'iOS Developer',
+  title: 'Developer',
   facts: {
     'GitHub': '<a href="https://github.com/BrenoAquino">BrenoAquino</a>',
     'LinkedIn': '<a href="www.linkedin.com/in/breno-aquino">BrenoAquino</a>',
-    'Email': '<a href="mailto:breno@brenoaquino.dev">breno@brenoaquino.dev</a>'
+    'Email': '<a href="mailto:brenoaquino7phr@gmail.com">brenoaquino7phr@gmail.com</a>'
   },
   overview: `
   I am a person who is always looking for innovation and automating everything possible, enabling us to focus on more important tasks. I have experience with projects in the areas of education, streaming and finance. I also have experience in creating SDKs, both for internal use and for sale to third parties. I am always open to discuss about architecture and performance.
@@ -29,14 +29,16 @@ module.exports = {
   positions: [
     {
       company: 'Rakuten',
-      location: 'United States • Remote',
+      location: 'United States/Canada • Remote',
       title: 'iOS Developer',
       period: 'October 2022 - current',
-      skills: ['iOS', 'Swift', 'Objective C', 'UIKit', 'View Code', 'SwiftUI', 'Unit Testing', 'XCTest', 'Modules', 'Github Actions', 'Xcode Cloud', 'Fastlane', 'SDK'],
+      skills: ['iOS', 'Swift', 'SwiftUI', 'Combine', 'Unit Testing', 'XCTest', 'Xcode Cloud', 'Fastlane', 'SDK'],
       contents: `
-* Responsible to design and create a custom new SDK for the analytics;
-* Implemented an authentication feature that is modified depending on the user's country;
-* Implemented a privacy module to internationalize the app and follow the laws of each country/state.
+* Responsible for creating an analytical SDK for internal use by the company with the aim of obtaining higher quality data to facilitate the mapping of product improvements. As the SDK could be called by any component at any time, it was necessary to create thread safe components, and to ensure this I created tests creating multiple threads and trying to access the resource at the same time. Test coverage for the SDK was around 92%;
+* The analytics SDK was also created to support different countries and each with its own domain. There is a buffer to avoid excessive requests and each buffer is linked to the country of the user who generated the events;
+* Worked on migrating authentication to using a WebView in which it talked to the application through JavaScript messages;
+* Improved the privacy flow with the aim of facilitating expansion to other states/countries that follow different privacy laws;
+* Responsible for creating components to serve as bridges between RxSwift - that we are removing from the project - and Combine. The objective was that new features and refactorings do not use third-party framworks, but to talk to existing components, the mentioned classes would be used.
 `
     },
     {
@@ -46,23 +48,27 @@ module.exports = {
       period: 'March 2022 - October 2022',
       skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'XCTest', 'Modules', 'Swift Package Manager', 'Cocoapods', 'Jenkins', 'Fastlane'],
       contents: `
-* Responsible for leading and mentoring a Junior developer, teaching and guiding on iOS development;
-* Implemented several individual modules and distributed using Cocoapods;
-* Responsible for maintaining the entire notification flow, composed of push notifications, deeplinks and a feature that allows the user to keep track of its notifications.
+* Responsible for leading and guiding a Junior developer with an emphasis on iOS development. I was responsible for meeting with him every week to answer questions and generally providing a collection of articles on the study topic selected at the time. We also communicate during the week to help with development and help with code reviews;
+* As the iOS team was made up of a significant number of developers and dozens of squads, we used several repositories for each module and a main one with the app focused on integrating and coordinating the app's navigation. All module distributions were used cocoapods;
+* To support multiple apps in the App Store and avoid duplicate code, I used different build schemas and configuration to define how some components/modules should behave and look;
+* Responsible for creating and maintaining multiple modules related to the app's notification/deeplink flow, the app used to have about 28.000.000 active user per month;
+* Worked on redesigning the app, where I could apply improvements to the app's architecture and making it more testable and maintainable.
 `
     },
     {
       company: 'Meta',
-      location: 'Canada • Remote',
+      location: 'United States/Canada • Remote',
       title: 'iOS Developer',
       period: 'December 2020 - February 2022 (part time)',
-      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'WebSocket', 'SDK', 'Cocoapods', 'CI/CD', 'Github Actions', 'Scrum'],
+      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Streaming', 'Quick', 'Nimble', 'XCTest', 'WebSocket', 'SDK', 'Cocoapods', 'CI/CD', 'Github Actions'],
       contents: `
 * Worked as a leader creating and defining the base structure project for the client Maple Leaf Sports & Entertainment (MLSE). Defining the priority of functionalities, architectures, APIs and web socket for real-time behavior;
-* Implemented an SDK following Clean and MVVM to provide features like live and VOD streams, chats, real-time stats and other features and distributed using Cocoapods;
-* Implemented a design system and a custom theme to be defined by the host app;
-* Implemented scripts and configured the CI (using Github Action) to automate the build and distribution of the compiled SDK and its documentation;
-* Used Agile and Scrum methodologies, also having weekly meetings with the client and the client backend team.
+* The aim of the project was to be integrable with any team app. For this, an interface was defined that the host app would implement to configure the SDK theme, being able to modify colors and logo in some locations;
+* To respond to events happening in the game in real time, it was necessary to create a web socket connection with the server. With this, the app could respond in real time with: trivia, advertising/celebration banners and mini games - using web view to load them.
+* A chat was also developed to send/receive messages among all users, as well as the possibility of creating groups - which would exist only for that game - to send/receive messages;
+* Used the RTSP protocol to load Live content, but we also provide compatibility for VOD content. The idea was publish the best moments of the game as VODs;
+* As it was an SDK used internally but could be sold for integration into third-party teams, it was necessary to configure CI/CD to publish an already compiled version of the SDK and its documentation in a public repository, along with a sample app to guide integration with the SDK;
+* Implement the SDK following MVVM to fulfill the mentioned requirements and provide good maintainability and scalability.
 `
     },
     {
@@ -70,42 +76,16 @@ module.exports = {
       location: 'São Paulo - SP, Brazil',
       title: 'iOS Developer',
       period: 'March 2019 - March 2022',
-      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'Swift Package Manager', 'CI/CD', 'Fastlane', 'Bitrise', 'SAFe'],
+      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Streaming', 'Quick', 'Nimble', 'XCTest', 'Swift Package Manager', 'CI/CD', 'Fastlane', 'Bitrise'],
       contents: `
-* Developed one stream project from the beginning and improved another streaming project, called NOW Online. Both of them provided live and VOD contents;
-* Worked as a leader of 2 other iOS developers defining the technologies, architecture and priorities;
-* Helped to define the entire solution and the architecture for the project, always looking for performance;
-* Implemented a feature to list all channels and their programs, to improve the performance and the time to render the screen to the user, I implemented a custom cache algorithm to avoid unnecessary recalculations and binary search to improve the time to find the current program based on the time;
-* Improved the architecture to use MVVM and follow Clean Architecture on the NOW project. This one had about 300,000+ monthly active users;
-* Increased the test coverage applying the dependency injection concept.
+* Worked on 2 streaming projects, one created from scratch and another that was already in production - with around 300.000 active users per month. Both of them provided live and VOD contents;
+* Implemented Fairplay DRM for live and VOD content. It was necessary to use two different logics since each one worked in a different way. I created an abstraction layer so that the player accepted different DRM implementations;
+* Handled different types of VODs such as: multi audio where the audio and subtitle track was disconnected from the media and the user could change any audio/subtitle track without reloading the media; single audio where the audio and subtitles were inserted directly into the media, to change the audio or subtitles it was necessary to load a new media with a pre-defined configuration, it was not possible to choose just the audio or just the subtitles, the user was forced to change both tracks; DVB where the subtitle was received in base64 and it was necessary to display it as an image over the content;
+* Implemented an architecture with multiple repositories to facilitate maintainability and scalability. There was a repository containing all the business rules and this was imported - using swift package manage - both into the iOS app repository and would be imported into the Apple TV app when it was developed;
+* All new features were covered by unit tests and aiming for 80% coverage;
+* Implemented a feature to list all channels and their programs, to improve the performance and the time to render the screen to the user, I implemented a custom cache algorithm to avoid unnecessary recalculations and binary search to improve the time to find the current program based on the time.
 `
     },
-    {
-      company: 'University of Fortaleza - UNIFOR',
-      location: 'Fortaleza - CE, Brazil',
-      title: 'iOS Developer',
-      period: 'March 2018 - March 2019',
-      skills: ['iOS', 'Swift', 'UIKit', 'Storyboard', 'View Code', 'UI Testing', 'MVC', 'MVVM', 'Tech Lead', 'Ruby on Rails', 'Fastlane'],
-      contents: `
-* Worked as a Tech Lead for a team of 10 interns;
-* Responsible for all the university iOS Apps. The main university app had about 24,000+ monthly active users;
-* Taught iOS courses for interns and students;
-* Responsible for teaching about the iOS, git, scrum methodology and also how to design the entire solution, from the UX, priorities, APIs, front end to tests;
-* Refactored the app following MVVM. Among the features, the student's enrollment, the class frequency and the campus map with all the university's landmarks stand out.
-`
-    },
-//     {
-//       company: 'University of Fortaleza - UNIFOR',
-//       location: 'Fortaleza - CE, Brazil',
-//       title: 'Intern - iOS Developer',
-//       period: 'July 2017 - March 2018',
-//       skills: ['iOS', 'Swift'],
-//       contents: `
-// * I learned more about iOS working on the main university app, especially working on the chat and the map feature that could guide the users through the university.
-// * Refactored the app following MVVM.
-// * After 9 months I was hired to be responsible for a team composed of 10 interns. My job was to teach them about iOS and software engineering.
-// `
-//     },
   ],
   education: [
     {
@@ -122,11 +102,6 @@ module.exports = {
     }
   ],
   personal_projects: [
-    // {
-    //   title: 'iOS Movies',
-    //   skills: ['iOS', 'Swift', 'SwiftUI', 'Design System', 'MVVM'],
-    //   contents: 'App integrated with TheMovieDB to be a base project to apply new technologies or create a POC.',
-    // },
     {
       title: 'iOS Cash Handler',
       skills: ['iOS', 'Swift', 'SwiftUI', 'Design System', 'MVVM', 'View Code'],
@@ -142,11 +117,6 @@ module.exports = {
       skills: ['Computer Vision', 'Python', 'Flask', 'WebSocket', 'MongoDB', 'Raspberry Pi', 'Computer Vision', 'Machine Learning'],
       contents: 'System to detect people and objects using IP cameras. It can be used with an alarm or for automation.'
     },
-    // {
-    //   title: 'C.E.I. B3 - Crawler',
-    //   skills: ['Web Scraping', 'NodeJS', 'Python', 'Serverless', 'AWS DynamoDB', 'AWS Lambda'],
-    //   contents: 'Crawler project to extract data from a Brazilian Stock Market platform and give you an overview about your investments by APIs.'
-    // },
     {
       title: 'Mobile Stores (App Store e Play Store) - Crawler',
       skills: ['Web Scraping', 'Python', 'Serverless', 'AWS Step Functions', 'AWS Lambda', 'AWS DynamoDB'],
