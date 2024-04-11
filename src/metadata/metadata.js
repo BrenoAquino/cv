83ce32ad65ae32654f272d12d1913e7f512036c6
BrenoAquino <brenoaquino7phr@gmail.com>
2024-04-11 18:28:02 -0300

    update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,117 +1,80 @@
 module.exports = {
-  name: 'Breno Aquino',
-  title: 'iOS Developer',
+  name: 'Paula Vitória Pereira Motoyama',
+  address: "Rua José Alves Cavalcante, 700, 1A, Cidade dos Funcionários. Fortaleza, Ceará, CEP 60822-570",
+  phone: '(85) 9 9959-3573',
   facts: {
-    'GitHub': '<a href="https://github.com/BrenoAquino">BrenoAquino</a>',
+    'Lattes': '<a href="http://lattes.cnpq.br/4007777657492212 ">Paula Motoyama</a>',
     'LinkedIn': '<a href="www.linkedin.com/in/breno-aquino">BrenoAquino</a>',
-    'Email': '<a href="mailto:breno@brenoaquino.dev">breno@brenoaquino.dev</a>'
+    'Email': '<a href="mailto:paula.pmotoyama@gmail.com">paula.pmotoyama@gmail.com</a>'
   },
   overview: `
   Estou em busca de uma oportunidade em auditoria médica, onde eu possa aplicar minhas habilidades e contribuir com minha experiência profissional e acadêmica para impulsionar os resultados e a qualidade da equipe.
 `,
   positions: [
     {
-      company: 'Médica da estratégia saúde da família',
-      location: 'Unidade Básica de Saúde Triângulo 1',
-      title: 'Chorozinho/Ce',
+      company: 'Unidade Básica de Saúde Triângulo 1',
+      location: 'Chorozinho/Ce',
+      title: 'Médica da estratégia saúde da família',
       period: 'Janeiro 2023 - Atual',
-      skills: ['iOS', 'Swift', 'Objective C', 'UIKit', 'View Code', 'SwiftUI', 'Unit Testing', 'XCTest', 'Modules', 'Github Actions', 'Xcode Cloud', 'Fastlane', 'SDK'],
       contents: `
-* Responsável por projetar e criar uma SDK personalizado para o analytics da empresa;
-* Implementei um recurso de autenticação que é modificado dependendo do país do usuário;
-* Implementei um módulo de privacidade para internacionalizar o aplicativo e seguir as leis de cada país/estado.
+* Bolsista do Programa Médicos pelo Brasil;
+* Conduzi uma campanha educativa sobre infecções sexualmente transmissíveis, incluindo o HIV, abordando formas de prevenção incluindo no uso de PREP e PEP, direcionada a uma população mais vulnerável;
+* Restabeleci o fluxo de busca ativa dos pacientes sintomáticos respiratórios na unidade básica de saúde objetivando melhorar nas taxas de diagnóstico de tuberculose na área adscrita;
+* Atendimento de pacientes hipertensos, diabéticos, acompanhamento pré-natal, puericultura e demandas gerais e espontâneas;
+* Atendimentos realizados na unidade básica, domicílio e em ponto de apoio dos pacientes moradores da área adscrita, abrangendo todas as faixas etárias.
 `
     },
     {
-      company: 'Itaú',
-      location: 'Brazil • Remote',
-      title: 'iOS Developer',
-      period: 'March 2022 - October 2022',
-      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'XCTest', 'Modules', 'Swift Package Manager', 'Cocoapods', 'Jenkins', 'Fastlane'],
+      company: 'Unidade de Pronto-Atendimento',
+      location: 'Eusébio/Ce',
+      title: 'Médica plantonista',
+      period: 'Abril 2022 - Atual',
       contents: `
-* Responsável por liderar e orientar um desenvolvedor Junior, ensinando e orientando no desenvolvimento iOS;
-* Implementei vários módulos individuais e distribuídos usando Cocoapods;
-* Responsável por manter todo o fluxo de notificações, composto por push notifications, deeplinks e uma funcionalidade que permite ao usuário acompanhar suas notificações.
+* Atendimento a crianças e adultos na UPA, no eixo verde, que envolve casos de menor gravidade. 
+* Avaliação de pacientes para determinar a gravidade de sua condição e a necessidade de internação ou encaminhamento para a sala vermelha.
+* Contribuí para a criação de um protocolo de atendimento para pacientes com AVC na UPA, garantindo uma abordagem rápida e eficaz para o tratamento dessa condição baseada em evidências;
+* Participei da comissão de óbitos auxiliando na análise dos óbitos e qualidade de informação dos atestados de óbitos.
 `
     },
     {
-      company: 'Meta',
-      location: 'Canada • Remote',
-      title: 'iOS Developer',
-      period: 'December 2020 - February 2022 (part time)',
-      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'WebSocket', 'SDK', 'Cocoapods', 'CI/CD', 'Github Actions', 'Scrum'],
+      company: 'Unidade Básica de Saúde Croatá',
+      location: 'Ocara/Ce',
+      title: 'Médica da estratégia saúde da família',
+      period: 'Julho 2022 - Dezembro 2022',
       contents: `
-* Atuei como líder criando e definindo o projeto de estrutura base para o cliente Maple Leaf Sports & Entertainment (MLSE). Definindo a prioridade de funcionalidades, arquiteturas utilizadas, métricas, APIs e web socket para comportamento em tempo real;
-* Implementei uma SDK seguindo Clean e MVVM para fornecer recursos como transmissões ao vivo e VOD, chats, estatísticas em tempo real, mini jogos e outros recursos e distribuído usando Cocoapods;
-* Implementei um design system e um tema personalizado a ser definido pelo aplicativo host;
-* Implementei scripts e configurei o CI (usando Github Action) para automatizar a construção e distribuição do SDK compilado e sua documentação;
-* Utilizei metodologias Agile e Scrum, realizando também reuniões semanais com o cliente e a equipe de backend do cliente.
+* Bolsista do Programa Médicos pelo Brasil;
+* Implementei um sistema de marcação de horários para consultas, permitindo que os pacientes agendem suas visitas com antecedência, reduzindo assim o tempo de espera e proporcionando uma distribuição mais equitativa dos horários disponíveis;
+* Coordenei juntamente com a equipe uma campanha do Novembro Azul, focada na saúde integral do homem, indo além do tradicional enfoque no toque retal e na solicitação do PSA (Antígeno Prostático Específico).      
 `
     },
     {
-      company: 'Claro',
-      location: 'São Paulo - SP, Brazil',
-      title: 'iOS Developer',
-      period: 'March 2019 - March 2022',
-      skills: ['iOS', 'Swift', 'UIKit', 'View Code', 'Unit Testing', 'Quick', 'Nimble', 'XCTest', 'Swift Package Manager', 'CI/CD', 'Fastlane', 'Bitrise', 'SAFe'],
-      contents: `
-* Desenvolvi um projeto de stream desde o início e aprimorei outro projeto de stream, chamado NOW Online. Ambos fornecem conteúdos ao vivo e VOD;
-* Trabalhei como líder de outros 2 desenvolvedores iOS definindo as tecnologias, arquitetura e prioridades;
-* Ajudei a definir a solução e a arquitetura do projeto, sempre buscando performance;
-* Implementei um recurso para listar todos os canais e seus programas, para melhorar o desempenho e o tempo de renderização da tela para o usuário, implementei um algoritmo de cache customizado para evitar recálculo desnecessários e busca binária para melhorar o tempo para encontrar o programa atual baseado em na hora atual;
-* Melhorou a arquitetura para usar MVVM e seguir o Clean Architecture no projeto NOW, além de ter começado a implementar testes unitários. Este aplicativo tinha cerca de 300.000+ usuários ativos mensais;
-* Aumentei a cobertura de testes aplicando e apliquei o conceito de injeção de dependência.
-`
-    },
-    {
-      company: 'University of Fortaleza - UNIFOR',
-      location: 'Fortaleza - CE, Brazil',
-      title: 'iOS Developer',
-      period: 'March 2018 - March 2019',
-      skills: ['iOS', 'Swift', 'UIKit', 'Storyboard', 'View Code', 'UI Testing', 'MVC', 'MVVM', 'Tech Lead', 'Ruby on Rails', 'Fastlane'],
-      contents: `
-* Atuei como Tech Lead para uma equipe de 10 estagiários;
-* Fui responsável por todos os aplicativos iOS da universidade. O principal aplicativo da universidade tinha mais de 24.000 usuários ativos mensais;
-* Ministrei cursos de iOS para estagiários e estudantes;
-* Responsável por ensinar sobre a metodologia iOS, git, scrum e também como desenhar toda a solução, desde UX, prioridades, APIs, front end até testes;
-* Liderei a refatoração do principal aplicativo da universidade seguindo MVVM. Dentre as funcionalidades, se destaca a matrícula do aluno, a frequência realizada pelo professor e o mapa do campus com todos os pontos de referência da universidade.
-`
-    },
+      company: 'Unidade Básica de Saúde Pajeú',
+      location: 'Pacajus/Ce',
+      title: 'Médica da estratégia saúde da família',
+      period: 'Maio 2022 - Julho 2022',
+      contents: ``
+    }
   ],
   education: [
     {
       company: 'University of Fortaleza - UNIFOR',
+      level: 'Mestrado',
+      course: 'Ciências Médicas',
+      period: 'Agosto 2022 - Atual',
+      contents: `
+* Linha de pesquisa: Vigilância sentinela das síndromes gripais no contexto da Atenção Primária à Saúde: fatores preditores do diagnóstico e prognóstico.
+`
+    },
+    {
+      company: 'University of Fortaleza - UNIFOR',
       level: 'Graduation',
       course: 'Computing Engineering',
-      period: '2015 - 2019',
+      period: '2016 - 2022',
       contents: `
-* Trabalho de Conclusão de Curso: Pesquisa usando uma rede neural recorrente para tentar predizer a tendência de criptomoedas.
-* Monitor da disciplina de Programação Orientada a Objetos em 2016.
-* Monitor da disciplina de Microprocessor em 2017.
-* Pesquisa na área de segurança em redes de sensores, tentando implementar um algoritmo para detecção de ataques.
+* Participei das seguintes ligas acadêmicas: patolôgia, infectolôgia, cancerolôgia, GO
+* Participei das monitorias volutárias de: ...
+* Participei das iniciação cientificade de: ...
 `
-    }
-  ],
-  personal_projects: [
-    {
-      title: 'iOS Cash Handler',
-      skills: ['iOS', 'Swift', 'SwiftUI', 'Design System', 'MVVM', 'View Code'],
-      contents: 'Esse projeto cria um aplicativo para gestão financeira com o objetivo de aplicar conhecimentos em SwiftUI, Coordinator, SPM e Clean Architecture.',
-    },
-    {
-      title: 'iOS Common Module',
-      skills: ['iOS', 'Swift', 'UIKit', 'Design System', 'View Code'],
-      contents: 'Um conjunto de extensões para views para melhorar a legibilidade de código, criação de constraints e permitindo uma fácil integração com um design system.',
-    },
-    {
-      title: 'Home Security System',
-      skills: ['Computer Vision', 'Python', 'Flask', 'WebSocket', 'MongoDB', 'Raspberry Pi', 'Computer Vision', 'Machine Learning'],
-      contents: 'Sistema de deteção de pessoas e objetos local utilizando câmera IP. Podendo ser utilizado com alarme ou para automações.'
-    },
-    {
-      title: 'Mobile Stores (App Store e Play Store) - Crawler',
-      skills: ['Web Scraping', 'Python', 'Serverless', 'AWS Step Functions', 'AWS Lambda', 'AWS DynamoDB'],
-      contents: 'Crawler para extrair reviews da App Store e Play Store e tentar extrair o sentimento (positivo ou negativo) do comentário.'
     }
   ]
 };
